c5858054c12601cb775d763e15c24b8b76b89910
Logan Martin <88mlogan@gmail.com>
2023-12-12 09:59:06 -0500

    Started to Imp TimeTravel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-function cl(q) {
-  console.log(q);
-}
-
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
@@ -12,12 +8,12 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares) || checkDraw(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -26,12 +22,25 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
+  const winner = calculateWinner(squares);
+  const draw = checkDraw(squares);
+
+  let status;
+  if (winner) {
+    status = "Congragulations " + winner;
+  } else if (draw) {
+    status = "It's a Draw";
+  } else {
+    status = "It's " + (xIsNext ? "X" : "O") + "'s turn";
+  }
+
+  console.log(squares);
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -48,5 +57,55 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function checkDraw(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] === null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay (nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
   );
 }
